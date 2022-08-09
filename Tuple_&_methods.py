d428bab1858c21a053200e83050d2ea00f75ef70
shivanineela <109790213+shivanineela@users.noreply.github.com>
2022-08-09 18:20:37 +0530

    Add files via upload

---
+++ Tuple_&_methods.py
@@ -0,0 +1,24 @@
+# x=(1)
+# print(x,type(x))
+
+# x=(1,)
+# print(x,type(x))
+
+# d=('apple','reliance','asus','dell','samsung')
+# d[1]="motorolla"
+
+# n="hello", "world"
+# print(n,type(n))
+# print(n[2])
+
+# v=(1,46.8,'hi',[2,5],('h','d'),None,True)
+# print(v,type(v))
+
+# v=(1,46.8,'hi',[2,5],('h','d'))
+# v[0]=34
+
+# v=(1,46.8,'hi',[2,5],('h','d'))
+# print(v.count(1))
+# print(v.index([2,5]))
+
+# print(dir(tuple))
